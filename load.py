8708ea57f23d72d9122a7ef7f3406a378c0347f1
ValdemarHH <valdemarheehansen+github@gmail.com>
2023-03-19 16:23:03 +0100

    Updated load so pixel range is {0, 1, ..., 255}

--- load.py
+++ load.py
@@ -9,13 +9,6 @@ df = pd.read_csv("Data\metadata.csv")
 data = np.asarray(df)
 attributes = np.array(df.columns)
 
-# print(data[0])
-# print(attributes)
-
-# print(data[0, 2])
-# print(data[0, 4])
-# print(data[0, -1])
-
 x1 = np.load(os.path.join(
     "Data", "singh_cp_pipeline_singlecell_images", data[0, 2], data[0, 4]))
 
@@ -25,16 +18,17 @@ for i in range(len(data)):
     file_path = os.path.join(
         "Data", "singh_cp_pipeline_singlecell_images", data[i, 2], data[i, 4])
     if os.path.exists(file_path):
-        x = np.load(file_path)
-        X.append(x)
+        image = np.load(file_path)
+        min_val, max_val = np.min(image). np.max(image)
+        pixel_range = max_val - min_val
+        scaled_pixels = (image - min_val) / pixel_range
+        scaled_pixels *= 255
+        converted_image = np.round(scaled_pixels).astype(np.uint8)
+        X.append(converted_image)
         y.append(data[i, -1])
-
-# X = [np.load(os.path.join("Data", "singh_cp_pipeline_singlecell_images", data[i, 2], data[i, 4])) for i in range(len(data))]
 
 X = np.array(X)
 y = np.array(y)
-
-# Complete_data_matrix = np.hstack((X, y[:, None]))
 
 np.savez("image_matrix.npz", images=X, labels=y)
 
