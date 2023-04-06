630b715be37a5531f0d39f05c97eb361c69e0f41
ValdemarHH <valdemarheehansen+github@gmail.com>
2023-04-06 13:35:02 +0200

    Updated VAE

--- VAE.py
+++ VAE.py
@@ -2,22 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import torchvision
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 def log_Categorical(x, theta, num_classes):
     x_one_hot = nn.functional.one_hot(x.long(), num_classes=num_classes)
     log_p = torch.log(theta)*x_one_hot
     return torch.sum(log_p, dim=1)
 
+
 class encoder(nn.Module):
-    def __init__(self, input_dim, latent_dim):
+    def __init__(self, input_dim, latent_dim, channels):
         super(encoder, self).__init__()
-        self.input_dim=input_dim
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, padding="same")
+        self.input_dim = input_dim
+        self.conv1 = nn.Conv2d(channels, 16, kernel_size=5, padding="same")
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, padding="same")
-        self.fully_connected = nn.Linear(32 * self.input_dim * self.input_dim, 2 * latent_dim)
+        self.fully_connected = nn.Linear(
+            32 * self.input_dim * self.input_dim, 2 * latent_dim)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -27,18 +31,21 @@ class encoder(nn.Module):
         x = x.view(-1, 32 * self.input_dim * self.input_dim)
         x = self.fully_connected(x)
         return x
-    
+
 
 class decoder(nn.Module):
-    def __init__(self, input_dim, latent_dim):
+    def __init__(self, input_dim, latent_dim, channels):
         super(decoder, self).__init__()
-        self.input_dim=input_dim
-        self.input = nn.Linear(latent_dim, 32 * self.input_dim * self.input_dim) 
+        self.input_dim = input_dim
+        self.channels = channels
+        self.input = nn.Linear(
+            latent_dim, 32 * self.input_dim * self.input_dim)
         self.conv1 = nn.ConvTranspose2d(
             32, 16, kernel_size=5, stride=1, padding=5 - (self.input_dim % 5))
         self.conv2 = nn.ConvTranspose2d(
-            16, 3, kernel_size=5, stride=1, padding=5 - (self.input_dim % 5))
-        self.fully_connected = nn.Linear(3 * self.input_dim * self.input_dim, 3 * self.input_dim * self.input_dim * 256)
+            16, channels, kernel_size=5, stride=1, padding=5 - (self.input_dim % 5))
+        self.fully_connected = nn.Linear(
+            channels * self.input_dim * self.input_dim, channels * self.input_dim * self.input_dim * 256)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
@@ -49,46 +56,46 @@ class decoder(nn.Module):
         x = nn.functional.relu(x)
         x = self.conv2(x)
         x = nn.functional.relu(x)
-        x = x.view(-1, 3 * 32 * 32)
+        x = x.view(-1, self.channels * self.input_dim * self.input_dim)
         x = self.fully_connected(x)
-        x = x.view(-1, 3 * self.input_dim * self.input_dim, 256)
+        x = x.view(-1, self.channels * self.input_dim * self.input_dim, 256)
         x = self.softmax(x)
         return x
 
 
 class VAE():
-    def __init__(self, X, pixel_range, latent_dim):
-        self.encoder = encoder(latent_dim)
-        self.decoder = decoder(latent_dim)
+    def __init__(self, X, pixel_range, latent_dim, input_dim, channels):
+        self.encoder = encoder(input_dim, latent_dim, channels)
+        self.decoder = decoder(input_dim, latent_dim, channels)
         self.pixel_range = pixel_range
 
         self.data_length = len(X)
-        self.prior = torch.distributions.MultivariateNormal(loc = torch.zeros(latent_dim), covariance_matrix = torch.eye(latent_dim))
-        
+        self.prior = torch.distributions.MultivariateNormal(
+            loc=torch.zeros(latent_dim), covariance_matrix=torch.eye(latent_dim))
+
     def encode(self, x):
         mu, std = torch.split(self.encoder.forward(x), 2, dim=1)
         return mu, std
 
     def reparameterization(self, mu, log_var, eps):
-        return mu + torch.exp(0.5*log_var) * eps 
+        return mu + torch.exp(0.5*log_var) * eps
 
     def decode(self, z):
         return self.decoder.forward(z)
-    
 
     def ELBO(self, x):
-        eps = torch.normal(mean = 0, std = torch.ones(len(x)))
+        eps = torch.normal(mean=0, std=torch.ones(len(x)))
         mu, log_var = self.encode(x)
         z = self.reparameterization(mu, log_var, eps)
         theta = self.decode(z)
-        log_posterior = torch.sum(torch.log(torch.distributions.MultivariateNormal(loc = mu, covariance_matrix = torch.diag(torch.exp(log_var)))), dim=1)
+        log_posterior = torch.sum(torch.log(torch.distributions.MultivariateNormal(
+            loc=mu, covariance_matrix=torch.diag(torch.exp(log_var)))), dim=1)
         log_prior = torch.sum(torch.log(self.prior), dim=1)
         log_con_like = log_Categorical(x, theta, self.pixel_range)
         reconstruction_error = torch.mean(log_con_like)
         regularizer = torch.mean(log_posterior - log_prior)
         elbo = -(reconstruction_error + regularizer)
         return elbo, reconstruction_error, regularizer
-
 
     def train(self, X, epochs, batch_size, lr=0.001):
         optimizer_encoder = torch.optim.SGD(self.encoder.parameters(), lr=lr)
@@ -106,16 +113,68 @@ class VAE():
                 elbo.backward()
                 optimizer_encoder.step()
                 optimizer_decoder.step()
-            print(f"Epoch: {epoch}, ELBO: {elbo}, Reconstruction Error: {reconstruction_error}, Regularizer: {regularizer}")
-        return self.encoder, self.decoder, reconstruction_errors, regularizers
+            if epochs == epoch - 1:
+                latent_space = self.reparameterization(self.encode(x))
+            print(
+                f"Epoch: {epoch}, ELBO: {elbo}, Reconstruction Error: {reconstruction_error}, Regularizer: {regularizer}")
+        return self.encoder, self.decoder, reconstruction_errors, regularizers, latent_space
 
 
-
-def generate_image(decoder, latent_dim, output_dim):
-    z = torch.normal(mean = 0, std = torch.ones(1, latent_dim))
+def generate_image(decoder, latent_dim, output_dim, channels):
+    z = torch.normal(mean=0, std=torch.ones(1, latent_dim))
     theta = decoder.forward(z)
     theta = theta.argmax(dim=1)
     theta = theta.view(3, output_dim, output_dim)
     theta = theta.detach().numpy()
-    plt.imshow(theta)
-    plt.show()
+    return theta
+
+
+latent_dim = 200
+epochs = 10
+batch_size = 1
+
+trainset = torchvision.datasets.MNIST(
+    root='./MNIST', train=True, download=True, transform=None)
+X = trainset.data.numpy()
+X = torch.tensor(X[:1000])
+
+# X = np.load("image_matrix.npz")["images"][:1000]
+
+pixel_range = 256
+input_dim = 28
+channels = 1
+
+VAE = VAE(X, pixel_range=pixel_range,
+          latent_dim=latent_dim, input_dim=input_dim, channels=channels)
+encoder, decoder, reconstruction_errors, regularizers, latent_space = VAE.train(
+    X=X, epochs=epochs, batch_size=batch_size)
+torch.save(encoder, "encoder.pt")
+torch.save(decoder, "decoder.pt")
+
+np.savez("latent_space.npz", latent_space=latent_space)
+
+plt.plot(reconstruction_errors, label="Reconstruction Error")
+plt.plot(regularizers, label="Regularizer")
+plt.xlabel("Epochs")
+plt.xticks(ticks=np.arange(0, epochs*len(X), len(X)),
+           labels=np.arange(0, epochs, 1))
+plt.title("ELBO Components")
+plt.legend()
+plt.show()
+
+output_dim = 28
+
+generated_images = []
+for _ in range(9):
+    image = generate_image(
+        decoder=decoder, latent_dim=latent_dim, output_dim=output_dim, chanels=channels)
+    generated_images.append(image)
+
+fig, ax = plt.subplots(3, 3)
+ax = ax.flatten()
+for i, image in enumerate(generated_images):
+    ax[i].imshow(image)
+    ax[i].set_xticks([])
+    ax[i].set_yticks([])
+plt.title("Generated images")
+plt.show()
