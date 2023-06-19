843380851c2d680f09ffa1824f30e9b0683799c4
nikolaj <nikolaj.hertz@yahoo.com>
2023-06-19 13:00:05 +0200

    changed visualization

--- visualisation.py
+++ visualisation.py
@@ -1,31 +1,66 @@
+############################################################################################################
+# visualisation.py
+# This script is to plot some of the training data (used in data section). 
+############################################################################################################
+
+
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.load('image_matrix.npz')
-images = data['images']
-labels = data['labels']
+train_size = 100_000
+test_size = 10_000
 
-print(images.shape)
-print(labels.shape)
+torch.backends.cudnn.deterministic = True
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+np.random.seed(42)
 
-MOAs, counts = np.unique(labels, return_counts=True)
+from DataLoader import BBBC
 
-[print(MOAs[i], ":", counts[i]) for i in range(len(MOAs))]
+main_path = "Data/"
 
-plt.imshow(images[0])
-plt.xticks([])
-plt.xticks([])
-plt.savefig('single-cell.png')
-plt.show()
+exclude_dmso = False
+shuffle = True
 
-fig, ax = plt.subplots(5, 3)
-ax = ax.flatten()
-for i, MOA in enumerate(MOAs):
-    image = images[labels == MOA][0]
-    ax[i].imshow(image)
-    ax[i].set_title(MOA)
-    ax[i].set_xticks([])
-    ax[i].set_yticks([])
-[ax[-j].set_visible(False) for j in range(1, len(ax)-i)]
-fig.suptitle("Cell image for each kind of MOA")
+subset = (train_size, test_size)
+
+dataset_train = BBBC(folder_path=main_path + "singh_cp_pipeline_singlecell_images",
+                        meta_path=main_path + "metadata.csv",
+                        subset=subset,
+                        test=False,
+                        exclude_dmso=exclude_dmso,
+                        shuffle=shuffle)
+
+dataset_test = BBBC(folder_path=main_path + "singh_cp_pipeline_singlecell_images",
+                        meta_path=main_path + "metadata.csv",
+                        subset=subset,
+                        test=True,
+                        exclude_dmso=exclude_dmso,
+                        shuffle=shuffle)
+
+classes_, indexes = np.unique(dataset_train.meta[dataset_train.col_names[-1]], return_index=True)
+boolean = len(classes_) == 13 if not exclude_dmso else len(classes_) == 12
+
+
+# fig, ax = plt.subplots(3,5, figsize=(15,10))
+
+import matplotlib.gridspec as gridspec
+gs = gridspec.GridSpec(2, 14)
+fig = plt.figure(figsize=(21, 6))
+
+for i in range(7):
+    ax1 = plt.subplot(gs[0, i*2:(i+1)*2], )
+    ax1.imshow(dataset_train[indexes[i]]['image'].reshape(68,68,3))
+    ax1.axis('off')
+    ax1.set_title(dataset_train[indexes[i]]['moa_name'], fontsize=14, fontweight='bold')    
+
+for i in range(6):
+    ax2 = plt.subplot(gs[1, (i*2+1):((i+1)*2+1)])
+    ax2.imshow(dataset_train[indexes[i+7]]['image'].reshape(68,68,3))
+    ax2.axis('off')
+    ax2.set_title(dataset_train[indexes[i+7]]['moa_name'], fontsize=14, fontweight='bold')
+gs.tight_layout(fig)
+# plt.savefig('images_from_train_set.png', dpi=300)
+print("The plot shown isn't the same as the plot saved. The saved plot is correct.!!")
 plt.show()
